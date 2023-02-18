42fab7904ef7efeafe858ffd95166621a1aac63f
JJKim <whdwls963@gmail.com>
2023-02-18 11:15:29 +0900

    230218 1115 jjk 알고리즘 문제풀이 환경 구축 , count p,y

--- Algorithm/src/com/Lv1/Count_P_Y.java
+++ Algorithm/src/com/Lv1/Count_P_Y.java
@@ -3,8 +3,28 @@ package com.Lv1;
 public class Count_P_Y {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String s = "Pyy"; 
+		boolean answer = true;
+		int cnty=0;
+		int cntp=0;
+				
+		String kys = s.toLowerCase();
+		System.out.println(kys);
+				
+		char[] array = kys.toCharArray();
+		System.out.println(array);
+		for(char i : array) {
+			if(i == 'y') {
+				cnty++;
+			}else if (i =='p') {
+				cntp++;
+			}
+		}
+		if(cntp != cnty) {
+			answer =false;
+		}
+		System.out.println(answer);
 
-	}
+	}  
 
 }
