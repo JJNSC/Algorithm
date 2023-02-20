5191f3482f301f846c0eae684ea11ff220a01944
JJKim <whdwls963@gmail.com>
2023-02-20 23:19:00 +0900

    230220 1118 jjk 하샤드 수

---
+++ Algorithm/src/com/Lv1/HashadeNum.java
@@ -0,0 +1,22 @@
+package com.Lv1;
+
+public class HashadeNum {
+	public static void main(String[] args) {
+		int x =10;
+		boolean answer = true;
+		int a=0;
+		int temp=x;
+		//System.out.println(x+","+a);
+		for(int i=0; temp>0 ;i++) {
+			a = a+(temp%10);
+			temp=temp/10;
+			}
+		if(x%a==0) {
+			answer=true;
+		}else {
+			answer=false;
+			}
+	    System.out.println(answer);
+
+	}  
+}
