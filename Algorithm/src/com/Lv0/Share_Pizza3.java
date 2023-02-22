005c30693bf1c76b0a8bb954883cbf57360b08f8
JJKim <whdwls963@gmail.com>
2023-02-23 00:28:03 +0900

    230223 1227 lv0 피자 나눠먹기(3)

---
+++ Algorithm/src/com/Lv0/Share_Pizza3.java
@@ -0,0 +1,13 @@
+package com.Lv0;
+
+public class Share_Pizza3 {
+
+	public static void main(String[] args) {
+		float slice = 7;
+		float n = 10;
+		
+		int answer = (int) Math.ceil(n/slice);
+		System.out.println(answer);
+	}
+
+}
