670b26a9ffdc1a9b654db876b7ef27db0909bf45
JJKim <whdwls963@gmail.com>
2023-02-23 00:47:52 +0900

    230223 1247 jjk 삼각형 만드는 정의인데 ...Array.sort()를 이용한..!

--- Algorithm/src/com/Lv0/ConditionToMakeRectangle.java
+++ Algorithm/src/com/Lv0/ConditionToMakeRectangle.java
@@ -1,27 +1,35 @@
 package com.Lv0;
 
+import java.util.Arrays;
+
 public class ConditionToMakeRectangle {
 
 	public static void main(String[] args) {
-		int[] sides = {1,2,3};
-		int result =2;
-		if( sides[0] >= sides[1] && sides[0]>=sides[2]) {
-			if(sides[0] < sides[1]+sides[2]) {
-				result = 1;
-			}
-		}else if ( sides[1] >= sides[2] && sides[1]>=sides[2]) {
-			if(sides[1] < sides[0]+sides[2]) {
-				result = 1;
-			}
-		}else if( sides[2] >= sides[0] && sides[2]>=sides[1]) {
-			if(sides[2] < sides[0]+sides[1]) {
-				result = 1;
-			}
-		}
+		int[] sides = {666,753,123};
 		
+		Arrays.sort(sides);
 		
+		int answer = sides[2] >= sides[0]+sides[1] ? 2 : 1;
+		System.out.println(answer);
 		
-		System.out.println(result);
+//		int result =2;
+//		if( sides[0] >= sides[1] && sides[0]>=sides[2]) {
+//			if(sides[0] < sides[1]+sides[2]) {
+//				result = 1;
+//			}
+//		}else if ( sides[1] >= sides[2] && sides[1]>=sides[2]) {
+//			if(sides[1] < sides[0]+sides[2]) {
+//				result = 1;
+//			}
+//		}else if( sides[2] >= sides[0] && sides[2]>=sides[1]) {
+//			if(sides[2] < sides[0]+sides[1]) {
+//				result = 1;
+//			}
+//		}
+//		
+//		
+//		
+//		System.out.println(result);
 
 	}
 
