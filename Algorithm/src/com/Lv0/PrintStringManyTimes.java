9e3b2962b6fa303670af64691910ee35c7c80deb
JJKim <whdwls963@gmail.com>
2023-02-24 01:01:02 +0900

    230224 0100 jjk
    문자반복 출력하기.
    문자열에 문자 더할때 그냥 + 를 쓰자

--- Algorithm/src/com/Lv0/PrintStringManyTimes.java
+++ Algorithm/src/com/Lv0/PrintStringManyTimes.java
@@ -3,18 +3,7 @@ package com.Lv0;
 public class PrintStringManyTimes {
 
 	public static void main(String[] args) {
-		String myString = "hello";
-		int n =3;
 		
-		String answer ="";
-		
-		for(int i =0; i <myString.length();i++) {
-			for(int j=0; j<n;j++) {
-				answer += myString.charAt(i);
-			}
-			
-		}
-		System.out.println(answer);
 
 	}
 
