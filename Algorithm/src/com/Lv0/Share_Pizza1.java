e861529fe9c6159b820c2e373b8944d903efb0fe
JJKim <whdwls963@gmail.com>
2023-02-24 23:42:04 +0900

    230224 2341 jjk 피자 나눠먹기 1

---
+++ Algorithm/src/com/Lv0/Share_Pizza1.java
@@ -0,0 +1,16 @@
+package com.Lv0;
+
+public class Share_Pizza1 {
+
+	public static void main(String[] args) {
+		int n =15;
+		int pizza=1;
+		
+		while(pizza*7< n ) {
+			pizza++;
+		}
+		System.out.println(pizza);
+
+	}
+
+}
