32f413ad44d20213642537a9356c4fbe6a6b8d44
JJKim <whdwls963@gmail.com>
2023-03-02 17:53:26 +0900

    230302 1753 jjk 짝수와홀수

---
+++ Algorithm/src/com/Lv0/Odd_And_Even.java
@@ -0,0 +1,16 @@
+package com.Lv0;
+
+public class Odd_And_Even {
+
+	public static void main(String[] args) {
+		int num = 3;
+		String answer = "";
+	        if(num%2==0){
+	            answer="Even";
+	        }else{
+	              answer="Odd";
+	        }
+	        System.out.println(num);
+	}
+
+}
