7025424e52a9e0a99989b1fb7b7af798fb66cc8c
JJKim <whdwls963@gmail.com>
2023-03-08 15:32:25 +0900

    230308 가위바위보 jjk

--- Algorithm/src/com/Lv0/AAA.java
+++
@@ -1,7 +0,0 @@
-package com.Lv0;
-
-public class AAA {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ Algorithm/src/com/Lv0/RockScissorsPaper.java
@@ -0,0 +1,19 @@
+package com.Lv0;
+
+public class RockScissorsPaper {
+    public static void main(String[] args) {
+        String rsp="205";
+        String answer="";
+
+        for(int i=0; i<rsp.length();i++){
+            if(rsp.charAt(i)=='2'){
+                answer += '0';
+            }else if(rsp.charAt(i)=='0'){
+                answer += '5';
+            }else if(rsp.charAt(i)=='5'){
+                answer += '2';
+            }
+        }
+        System.out.println(answer);
+    }
+}
