ac141129ad3b24be07907d01c73e1480b15c90f3
JJKim <whdwls963@gmail.com>
2023-03-10 14:53:00 +0900

    230310 jjk 세균증식

---
+++ Algorithm/src/com/Lv0/Propagate_Virus.java
@@ -0,0 +1,13 @@
+package com.Lv0;
+
+public class Propagate_Virus {
+    public static void main(String[] args) {
+        int n =7;
+        int t =15;
+
+        for(int i=0;i<t;i++){
+            n = n*2;
+        }
+        System.out.println(n);
+    }
+}
