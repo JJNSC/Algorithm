febbe7b69420c6984545475817eb5fab6306c03a
JJKim <whdwls963@gmail.com>
2023-03-12 14:38:42 +0900

    230312 jjk 주사위의 숫자

---
+++ Algorithm/src/com/Lv0/NumsOfDices.java
@@ -0,0 +1,13 @@
+package com.Lv0;
+
+public class NumsOfDices {
+    public static void main(String[] args) {
+        int[] box = {10,8,6};
+        int n=3;
+        int answer =1;
+        for(int i=0; i<box.length;i++){
+            answer *= box[i]/n;
+        }
+        System.out.println(answer);
+    }
+}
