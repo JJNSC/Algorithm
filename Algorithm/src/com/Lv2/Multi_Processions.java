5ff1317c16354e525fca99dc5052103f808a8bc0
JJKim <whdwls963@gmail.com>
2023-03-16 23:54:49 +0900

    230316 jjk

--- Algorithm/src/com/Lv2/Multi_Processions.java
+++ Algorithm/src/com/Lv2/Multi_Processions.java
@@ -5,10 +5,12 @@ public class Multi_Processions {
         int[][] arr1 = {{2, 3, 2}, {4, 2, 4}, {3, 1, 4}};
         int[][] arr2 = {{5, 4, 3}, {2, 4, 1}, {3, 1, 1}};
 
-        int[][] answer = new int[arr1.length][arr2.length];
-        for(int i=0; i<arr1.length;i++){
-            for(int j=0; j<arr2.length;j++){
-                answer[i][j] = 
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        for(int i=0; i<arr1[0].length;i++){
+            for(int j=0; j<arr2[0].length;j++){
+               for(int k=0; k<arr2.length;k++){
+                    answer[i][j] += arr1[i][k]* arr2[k][j];
+               }
             }
         }
     }
